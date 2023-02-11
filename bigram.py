3e763db475f1cbbdcc59d8ee5ace9f850a4852dc
Rick Lin <yanfulin@gmail.com>
2023-02-11 22:30:20 +0800

    comment about pytorch install

--- bigram.py
+++ bigram.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+#install torch in virtualenv
 
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
